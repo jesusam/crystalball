d5c7d34e6b7c4564d3bcab5ff862f21aeb4723b9
Jesus Morales <jesusam@umich.edu>
2013-05-07 10:05:18 -0400

    Final Crystal Ball project. Could make shake to predict the default image to improve.

--- CrystallBall/ViewController.h
+++ CrystallBall/ViewController.h
@@ -14,7 +14,7 @@
 
 @property (strong, nonatomic) IBOutlet UILabel *predictionLabel;
 @property (strong, nonatomic) NSArray *predictionArray;
-
-- (IBAction)buttonPressed:(UIButton *)sender;
+@property (strong, nonatomic) UIImageView *imageView;
+- (void) makePrediction;
 
 @end
